0c7daa4e501cffd32f90d6be8c999ea84ccd933a
Bruno Diaz <brunodiazll205@gmail.com>
2025-05-19 12:10:31 -0300

    Primer commit

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,9 @@
+function ItemListContainer({ mensaje }) {
+  return (
+    <div className="item-list-container">
+      <h2>{mensaje}</h2>
+    </div>
+  );
+}
+
+export default ItemListContainer;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import CartWidget from "./CartWidget";
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <h1>ZEUS</h1>
+      <ul className="nav-links">
+        <li>
+          <a href="#">Inicio</a>
+        </li>
+        <li>
+          <a href="#">Productos</a>
+        </li>
+        <li>
+          <a href="#">Contacto</a>
+        </li>
+      </ul>
+      <CartWidget />
+    </nav>
+  );
+}
+
+export default NavBar;
