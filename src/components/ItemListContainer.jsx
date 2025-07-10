cb31e5f09d90afc528c2bfbf780b6c21883ce822
Bruno Diaz <brunodiazll205@gmail.com>
2025-07-10 20:08:40 -0300

    Commit

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,18 +1,39 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { getProducts, getProductsByCategory } from "../data/products";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebaseConfig";
 import ItemList from "./ItemList";
 
 const ItemListContainer = () => {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { categoryId } = useParams();
 
   useEffect(() => {
-    const asyncFunc = categoryId ? getProductsByCategory : getProducts;
-    asyncFunc(categoryId).then((response) => {
-      setItems(response);
-    });
+    setLoading(true);
+
+    const productsRef = collection(db, "products");
+    const q = categoryId
+      ? query(productsRef, where("category", "==", categoryId))
+      : productsRef;
+
+    getDocs(q)
+      .then((res) => {
+        const productos = res.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setItems(productos);
+      })
+      .catch((error) => {
+        console.error("Error al traer productos", error);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }, [categoryId]);
+
+  if (loading) return <p>Cargando productos...</p>;
 
   return (
     <div style={{ padding: "2rem" }}>
